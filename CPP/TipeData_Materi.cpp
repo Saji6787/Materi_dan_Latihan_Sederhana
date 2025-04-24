39a54acbdbe962dce3580049adfe3a9fe3c83a07
maytanaya <hasnamaytanaya@gmail.com>
2025-04-24 07:19:31 +0700

    Update TipeData_Materi.cpp

---
+++ CPP/TipeData_Materi.cpp
@@ -0,0 +1,14 @@
+---Data Types---
+int year = 2024;
+double pi = 3.14159;
+char mention = '@';
+std::string message = "haloww"; (string)
+bool late_to_eat = true;
+
+---Declare a Variable---
+1. They are case-sensitive
+2. cannot contain whitespaces or special characters 
+3. must begin with a letter, can't start with a number
+
+const int minutes = 60; 
+(const -> unchangeable and read-only)
